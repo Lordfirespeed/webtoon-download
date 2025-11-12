e3da31892aeb3f225334f31c4290e4a23e6be6e5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-11-12 03:24:46 +0000

    auto-populate `Series` metadata from title_no

--- src/webtoon_download/metadata.py
+++ src/webtoon_download/metadata.py
@@ -1,18 +1,35 @@
 from dataclasses import dataclass, field
 
 from aiopath import AsyncPath
+from bs4 import BeautifulSoup
 from yarl import URL
+
+from webtoon_download.context import AppContext
 
 
 @dataclass
 class Series:
     title_no: int
-    title: str | None = field(default=None)
-    slug: str | None = field(default=None)
+    title: str
+    slug: str
+    base_url: URL
 
     @property
-    def indirect_url(self):
-        return URL(f"https://www.webtoons.com/en/genre/series/list?title_no={self.title_no}")
+    def list_url(self) -> URL:
+        return (self.base_url / f"list").with_query({"title_no": self.title_no})
+
+    @classmethod
+    async def get_populated_series(cls, title_no: int, context: AppContext):
+        indirect_url = URL(f"https://www.webtoons.com/en/genre/series/list?title_no={title_no}")
+        response = await context.session.get(indirect_url)
+        if not response.ok:
+            raise Exception("response not ok")
+        base_url = response.url.parent
+        body = await response.text()
+        soup = BeautifulSoup(body, features="html.parser")
+        title_raw = soup.find("title").text
+        title = title_raw[:title_raw.rindex("|")].strip()
+        return cls(title_no=title_no, title=title, slug=base_url.name, base_url=base_url)
 
 
 @dataclass
