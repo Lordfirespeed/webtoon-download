ba8a01dae8de19a46d865a1cf11be1efc5bf9ea5
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-11-12 03:02:52 +0000

    unfreeze metadata dataclasses for simplicity

--- src/webtoon_download/metadata.py
+++ src/webtoon_download/metadata.py
@@ -4,26 +4,29 @@ from aiopath import AsyncPath
 from yarl import URL
 
 
-@dataclass(frozen=True)
+@dataclass
 class Series:
     title_no: int
+    title: str | None = field(default=None)
+    slug: str | None = field(default=None)
 
     @property
     def indirect_url(self):
         return URL(f"https://www.webtoons.com/en/genre/series/list?title_no={self.title_no}")
 
 
-@dataclass(frozen=True)
+@dataclass
 class Episode:
     series: Series
     index: int
+    title: str | None = field(default=None)
 
     @property
     def indirect_url(self):
         return URL(f"https://www.webtoons.com/en/genre/series/episode/viewer?title_no={self.series.title_no}&episode_no={self.index}")
 
 
-@dataclass(frozen=True)
+@dataclass
 class EpisodePage:
     episode: Episode
     index: int
