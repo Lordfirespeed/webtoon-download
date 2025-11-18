f26fba7a0e21a1f46c4411c7680b78ce204c10c1
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2025-11-18 00:02:29 +0000

    this *does* work and it's less verbose, yay

--- src/webtoon_download/util/pydantic_aiopath.py
+++ src/webtoon_download/util/pydantic_aiopath.py
@@ -1,15 +1,9 @@
-import typing
 from pathlib import Path
-from typing import Annotated, Any
+from typing import Any, Annotated
 
 from aiopath import AsyncPath
-from pydantic_core import core_schema, PydanticCustomError, PydanticSerializationUnexpectedValue
-from pydantic import (
-    GenerateSchema,
-    GetCoreSchemaHandler,
-    GetJsonSchemaHandler,
-    ValidationError,
-)
+from pydantic import GetCoreSchemaHandler
+from pydantic_core import core_schema
 
 
 # see https://docs.pydantic.dev/latest/concepts/types/#handling-third-party-types (accessed 2025-11-17)
@@ -21,43 +15,21 @@ class _AsyncPathPydanticAnnotation:
         _handler: GetCoreSchemaHandler,
     ) -> core_schema.CoreSchema:
 
-        origin = typing.get_origin(_source_type)
-        path_constructor = origin
-        strict_inner_schema = core_schema.str_schema(strict=True)
-        lax_inner_schema = core_schema.str_schema()
+        def validate_from_path(value: Path) -> AsyncPath:
+            return AsyncPath(value)
 
-        def path_validator(input_value: str):
-            try:
-                if not isinstance(input_value, str):
-                    raise PydanticCustomError("path_type", "Input is not a valid path")
-                return path_constructor(input_value)
-            except TypeError as e:
-                raise PydanticCustomError("path_type", "Input is not a valid path") from e
-
-        def serialize_path(path: Any, info: core_schema.SerializationInfo):
-            if not isinstance(path, (origin, str)):
-                raise PydanticSerializationUnexpectedValue(
-                    f"Expected `{origin}` but got `{type(path)}` with value `'{path}'` - serialized value may not be as expected."
-                )
-            if info.mode == "python":
-                return path
-            return str(path)
-
-        instance_schema = core_schema.json_or_python_schema(
-            json_schema=core_schema.no_info_after_validator_function(path_validator, lax_inner_schema),
-            python_schema=core_schema.is_instance_schema(origin),
-        )
-
-        schema = core_schema.lax_or_strict_schema(
-            lax_schema=core_schema.union_schema(
+        from_path_schema = core_schema.no_info_after_validator_function(validate_from_path, _handler.generate_schema(Path))
+        return core_schema.json_or_python_schema(
+            json_schema=from_path_schema,
+            python_schema=core_schema.union_schema(
                 [
-                    instance_schema,
-                    core_schema.no_info_after_validator_function(path_validator, strict_inner_schema),
-                ],
-                custom_error_type="path_type",
-                custom_error_message=f"Input is not a valid path for {origin}",
-            ),
-            strict_schema=instance_schema,
-            serialization=core_schema.plain_serializer_function_ser_schema(serialize_path, info_arg=True, when_used="always"),
+                core_schema.is_instance_schema(AsyncPath),
+                from_path_schema,
+            ]),
+            serialization=core_schema.plain_serializer_function_ser_schema(lambda instance: str(instance))
         )
-        return schema
+
+
+type PydanticAsyncPath = Annotated[AsyncPath, _AsyncPathPydanticAnnotation]
+
+__all__ = ("PydanticAsyncPath",)
